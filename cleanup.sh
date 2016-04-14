82fbec1ee2230b4d39dbf72414beca8f40144dc6
Vincent <meijer.vincent@gmail.com>
2016-04-14 11:39:08 -0400

    Extra line to remove volumes (commented out for safety)

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,6 @@
 docker rm $(docker ps -q -f status=exited)
 
 docker rmi $(docker images -q -f dangling=true)
+
+# Remove dangling volumes (carefull...)
+# docker volume rm $(docker volume ls -qf dangling=true)
