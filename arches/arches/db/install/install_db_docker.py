4452aed26816f3cecab9585582b9794ba53e0177
Vincent <meijer.vincent@gmail.com>
2016-04-21 17:44:28 -0400

    Printing extra info

--- arches/arches/db/install/install_db_docker.py
+++ arches/arches/db/install/install_db_docker.py
@@ -10,25 +10,27 @@ def run_initial_sql():
 
     # Generate a sql file that sources all necessary sql files into one file
     buffer = ''
-    buffer += "\n-- Run all the sql scripts in the dependencies folder\n"
+    buffer += "\n\echo Run all the sql scripts in the dependencies folder\n"
     for infile in glob.glob(posixpath.join(db_directory, 'install', 'dependencies', '*.sql')):
         buffer += source(infile.replace("\\", posixpath.sep))
 
-    buffer += "\n-- Reload all managed schemas\n"
+    buffer += "\n\echo Run all the sql scripts in the ddl folder\n"
     for infile in glob.glob(posixpath.join(db_directory, 'ddl', '*.sql')):
         buffer += source(infile.replace("\\", posixpath.sep))
 
-    buffer += "\n-- Add all the data in the dml folder\n"
+    buffer += "\n\echo Run all the sql scripts in the dml folder\n"
     for infile in glob.glob(posixpath.join(db_directory, 'dml', '*.sql')):
         buffer += source(infile.replace("\\", posixpath.sep))
 
-    buffer += "\n-- Run all the sql in teh postdeployment folder\n"
+    buffer += "\n\echo Run all the sql scripts in the postdeployment folder\n"
     for infile in glob.glob(posixpath.join(db_directory, 'install', 'postdeployment', '*.sql')):
         buffer += source(infile.replace("\\", posixpath.sep))
 
-    buffer += "\n-- Spring cleaning\n"
-    buffer += "VACUUM ANALYZE;\n"
+    buffer += "\n\echo Spring cleaning\n"
+    buffer += "\n VACUUM ANALYZE\n"
 
+    buffer += "\n\echo Done installing all sql files;\n"
+	
     write_to_file(path_to_file, buffer)
 
     os.system('psql -U postgres -d template_postgis_20 -f %s' % path_to_file)
@@ -38,7 +40,7 @@ def write_to_file(fileName, contents, mode='w', encoding='utf-8', **kwargs):
     ensure_dir(fileName)
     file = codecs.open(fileName, mode=mode, encoding=encoding, **kwargs)
     file.write(contents)
-    os.system('chmod -R 777 ' + fileName)
+    # os.system('chmod -R 777 ' + fileName)
     file.close()
 
 
