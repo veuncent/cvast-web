3f568c51b2203498229d9914a38351db0b0f5daf
Vincent <meijer.vincent@gmail.com>
2016-06-24 18:48:32 +0200

    testcommit

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker build -f Dockerfile-arches-complete -t cvast/arches-complete ./ 
+sudo docker build -f Dockerfile-arches-complete -t cvast/arches-complete ./  
 sudo docker build -f Dockerfile-web -t cvast/cvast-web ./
 sudo docker build -f Dockerfile-db -t cvast/cvast-db ./
 sudo docker build -f Dockerfile-elasticsearch -t cvast/cvast-elasticsearch ./
