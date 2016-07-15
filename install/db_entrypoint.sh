1bb6306692a54ea98be55e5c3ffcfea48ccd6437
Vincent <meijer.vincent@gmail.com>
2016-07-15 14:59:23 -0400

    Ref #22 Changes to permissions/ownership

--- install/db_entrypoint.sh
+++ install/db_entrypoint.sh
@@ -3,19 +3,21 @@ init_datadir() {
 	echo "Initializing data dir..."
 	if [[ -d ${DATA_VOLUME} ]]; then
 		if [ "$(ls -A ${DATA_VOLUME} 2>/dev/null)" ]; then
-			echo "Host folder already initialized. Skipping copy..."
+			echo "Host folder not empty. Skipping copy..."
 		else
 			echo "Copying ${PG_DATADIR} to ${DATA_VOLUME}"
-			cp -R ${PG_DATADIR}/* ${DATA_VOLUME}
-			echo "Changing permissions of all files in ${DATA_VOLUME} to 0600"
-			find ${DATA_VOLUME} -type f -exec chmod 0600 {} \;
-			
-			echo "Changing permissions of all folders in ${DATA_VOLUME} to 0700"
-			find ${DATA_VOLUME} -type d -exec chmod 0700 {} \;
-			
-			echo "Changing ownership of ${DATA_VOLUME} to ${PG_USER}"
-			chown -R ${PG_USER}:${PG_USER} ${DATA_VOLUME}
+			cp -R ${PG_DATADIR}/* ${DATA_VOLUME}			
 		fi	
+		
+		echo "Changing permissions of all files in ${DATA_VOLUME} to 0600"
+		find ${DATA_VOLUME} -type f -exec chmod 0600 {} \;
+		
+		echo "Changing permissions of all folders in ${DATA_VOLUME} to 0700"
+		find ${DATA_VOLUME} -type d -exec chmod 0700 {} \;
+		
+		echo "Changing ownership of ${DATA_VOLUME} to ${PG_USER}"
+		chown -R ${PG_USER}:${PG_USER} ${DATA_VOLUME}
+
 	else
 		echo "!!! Data volume does not exist !!!"
 	fi
@@ -25,11 +27,24 @@ init_configdir() {
 	echo "Initializing config dir..."
 	if [[ -d ${CONFIG_VOLUME} ]]; then
 		if [ "$(ls -A ${CONFIG_VOLUME} 2>/dev/null)" ]; then
-			echo "Host folder already initialized. Skipping copy..."
+			echo "Host folder not empty. Skipping copy..."
 		else
-			echo "Copying ${PG_CONFIGDIR} to ${CONFIG_VOLUME}, preserving permissions"
-			cp -p -R ${PG_CONFIGDIR}/* ${CONFIG_VOLUME}
+			echo "Copying ${PG_CONFIGDIR} to ${CONFIG_VOLUME}"
+			cp -R ${PG_CONFIGDIR}/* ${CONFIG_VOLUME}
 		fi	
+		
+		echo "Changing permissions of all files in ${DATA_VOLUME} to 0600"
+		find ${DATA_VOLUME} -type f -exec chmod 0600 {} \;
+		
+		echo "Changing permissions of all folders in ${DATA_VOLUME} to 0700"
+		find ${DATA_VOLUME} -type d -exec chmod 0700 {} \;
+		
+		echo "Setting ownership and permissions on ${CONFIG_VOLUME}"
+		chown -R ${PG_USER}:${PG_USER} ${CONFIG_VOLUME}
+		chown root:root ${CONFIG_VOLUME}/postgresql.conf
+        chown root:root ${CONFIG_VOLUME}/pg_hba.conf
+		chmod 666 ${CONFIG_VOLUME}/postgresql.conf
+		chmod 666 ${CONFIG_VOLUME}/pg_hba.conf
 	else
 		echo "!!! Data volume does not exist !!!"
 	fi
