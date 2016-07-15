d72dbeba3850ce87efbf5999eadeecbe39b46d4c
Vincent <meijer.vincent@gmail.com>
2016-07-15 10:57:48 -0400

    Ref #22 Refactoring and missing white space

--- install/db_entrypoint.sh
+++ install/db_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 init_datadir() {
 	echo "Initializing datadir..."
-	if [ "$(ls -A ${DATA_VOLUME} 2>/dev/null)"]; then
-		echo "Host folder already initialized. Skipping copy..."
-	else
-		echo "Copying ${PG_DATADIR} to ${DATA_VOLUME}"
-		cp -R ${PG_DATADIR}/* ${DATA_VOLUME}
-		if [[ -d ${DATA_VOLUME} ]]; then
+	if [[ -d ${DATA_VOLUME} ]]; then
+		if [ "$(ls -A ${DATA_VOLUME} 2>/dev/null)" ]; then
+			echo "Host folder already initialized. Skipping copy..."
+		else
+			echo "Copying ${PG_DATADIR} to ${DATA_VOLUME}"
+			cp -R ${PG_DATADIR}/* ${DATA_VOLUME}
 			echo "Changing permissions of all files in ${DATA_VOLUME} to 0600"
 			find ${DATA_VOLUME} -type f -exec chmod 0600 {} \;
 			
@@ -16,16 +16,20 @@ init_datadir() {
 			echo "Changing ownership of ${DATA_VOLUME} to ${PG_USER}"
 			chown -R ${PG_USER}:${PG_USER} ${DATA_VOLUME}
 		fi	
-	fi	
+	else
+		echo "!!! Data volume does not exist !!!"
 }
 
 init_logdir() {
   echo "Initializing logdir..."
+  echo "Changing permissions of all files and folders in ${PG_LOGDIR} to 1775"
   chmod -R 1775 ${PG_LOGDIR}
+  echo "Changing ownership of ${PG_LOGDIR} to root:${PG_USER}"
   chown -R root:${PG_USER} ${PG_LOGDIR}
 }
 
 set_password() {
+	su - ${PG_USER} &&\
 	/etc/init.d/postgresql start &&\
 	psql -d postgres -c "ALTER USER ${PG_USER} with encrypted password '${DB_PASS}';" &&\
 	/etc/init.d/postgresql stop
@@ -37,4 +41,5 @@ change_user_to_postgres() {
 }
 
 init_datadir
+init_logdir
 #change_user_to_postgres
