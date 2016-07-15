d1cd59817663f7384f33ceb072d36b2b7eb06bb8
Vincent <meijer.vincent@gmail.com>
2016-07-15 13:36:09 -0400

    Ref #22 Added config and log volume initialization

--- install/db_entrypoint.sh
+++ install/db_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 init_datadir() {
-	echo "Initializing datadir..."
+	echo "Initializing data dir..."
 	if [[ -d ${DATA_VOLUME} ]]; then
 		if [ "$(ls -A ${DATA_VOLUME} 2>/dev/null)" ]; then
 			echo "Host folder already initialized. Skipping copy..."
@@ -21,26 +21,40 @@ init_datadir() {
 	fi
 }
 
-init_logdir() {
-  echo "Initializing logdir..."
-  echo "Changing permissions of all files and folders in ${PG_LOGDIR} to 1775"
-  chmod -R 1775 ${PG_LOGDIR}
-  echo "Changing ownership of ${PG_LOGDIR} to root:${PG_USER}"
-  chown -R root:${PG_USER} ${PG_LOGDIR}
+init_configdir() {
+	echo "Initializing config dir..."
+	if [[ -d ${CONFIG_VOLUME} ]]; then
+		if [ "$(ls -A ${CONFIG_VOLUME} 2>/dev/null)" ]; then
+			echo "Host folder already initialized. Skipping copy..."
+		else
+			echo "Copying ${PG_CONFIGDIR} to ${CONFIG_VOLUME}, preserving permissions"
+			cp -p -R ${PG_CONFIGDIR}/* ${CONFIG_VOLUME}
+		fi	
+	else
+		echo "!!! Data volume does not exist !!!"
+	fi
 }
+
+init_logdir() {
+	echo "Initializing log dir..."
+	if [[ -d ${LOG_VOLUME} ]]; then
+		echo "Changing permissions of all files and folders in ${LOG_VOLUME} to 1775"
+		chmod -R 1775 ${LOG_VOLUME}
+		echo "Changing ownership of ${LOG_VOLUME} to root:${PG_USER}"
+		chown -R root:${PG_USER} ${LOG_VOLUME}
+	else
+		echo "!!! Log volume does not exist !!!"
+	fi
+}
+
 
 set_password() {
 	su - ${PG_USER} &&\
 	/etc/init.d/postgresql start &&\
-	psql -d postgres -c "ALTER USER ${PG_USER} with encrypted password '${DB_PASS}';" &&\
+	psql -d ${PG_DBNAME} -c "ALTER USER ${PG_USER} with encrypted password '${PG_PASSWORD}';" &&\
 	/etc/init.d/postgresql stop
 }
 
-change_user_to_postgres() {
-	echo "Switching to user ${PG_USER}"
-	su - ${PG_USER}
-}
-
 init_datadir
+init_configdir
 init_logdir
-#change_user_to_postgres
