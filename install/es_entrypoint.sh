387c22ebbc4de1bfe013a008cb888f4ca16793ca
Vincent <meijer.vincent@gmail.com>
2016-07-21 16:14:33 -0400

    Ref #34 Added copying of data folder to mounted volume

--- install/es_entrypoint.sh
+++ install/es_entrypoint.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+	
+			
+init_datadir() {
+	echo "Initializing Elasticsearch data dir..."
+	if [[ -d ${DATA_VOLUME} ]]; then
+		# Do only if told explicitly: copies files into the persistence filesystem. (Use with caution)
+		if [[ ${IS_CLEAN_ENV} == true ]]; then
+			echo "Clean environment. Copying to mounted volume..."
+			if [ "$(ls -A ${DATA_VOLUME} 2>/dev/null)" ]; then
+				echo "Host folder not empty. Skipping copy..."
+			else
+				echo "Copying ${ES_DATADIR} to ${DATA_VOLUME}"
+				cp -R ${ES_DATADIR}/* ${DATA_VOLUME}			
+			fi
+		else
+			echo "Existing environment, not copying anything to mounted volume..."
+		fi
+	else
+		echo "!!! Data volume does not exist !!!"
+		exit 1
+	fi
+}
+
 init_configdir() {
 	echo "Initializing Elasticsearch config dir..."
 	if [[ -d ${CONFIG_VOLUME} ]]; then
@@ -16,7 +40,11 @@ init_configdir() {
 		fi
 	else
 		echo "!!! Data volume does not exist !!!"
+		exit 1
 	fi
 }
 
+echo "*** Initializing Elasticsearch ***"
+
+init_datadir
 init_configdir
