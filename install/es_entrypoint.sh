0b456f79e246947b3a4a9bbbbdfa2752a4e287a2
Vincent <meijer.vincent@gmail.com>
2016-07-21 13:55:56 -0400

    Ref #34 Added volumes, refactoring

--- install/es_entrypoint.sh
+++ install/es_entrypoint.sh
@@ -2,12 +2,18 @@
 init_configdir() {
 	echo "Initializing Elasticsearch config dir..."
 	if [[ -d ${CONFIG_VOLUME} ]]; then
-		if [ "$(ls -A ${CONFIG_VOLUME} 2>/dev/null)" ]; then
-			echo "Host folder not empty. Skipping copy..."
+		# Do only if told explicitly: copies files into the persistence filesystem. (Use with caution)
+		if [[ ${IS_CLEAN_ENV} == true ]]; then
+			echo "Clean environment. Copying to mounted volume..."
+			if [ "$(ls -A ${CONFIG_VOLUME} 2>/dev/null)" ]; then
+				echo "Host folder not empty. Skipping copy..."
+			else
+				echo "Copying elasticsearch.yml to ${CONFIG_VOLUME}, preserving permissions"
+				cp -p ${INSTALL_DIR}/elasticsearch.yml ${CONFIG_VOLUME}/elasticsearch.yml
+			fi	
 		else
-			echo "Copying elasticsearch.yml to ${CONFIG_VOLUME}, preserving permissions"
-			cp -p ${INSTALL_DIR}/elasticsearch.yml ${CONFIG_VOLUME}/elasticsearch.yml
-		fi	
+			echo "Existing environment, not copying anything to mounted volume..."
+		fi
 	else
 		echo "!!! Data volume does not exist !!!"
 	fi
