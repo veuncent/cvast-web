a45aee437daad061ce17439949e7e38bfd396238
Vincent <meijer.vincent@gmail.com>
2016-08-01 15:53:04 -0400

    Ref #26 Made Elasticsearch ready to run as cluster. Also changed from running as root to running as elasticsearch user

--- install/elasticsearch/es_entrypoint.sh
+++ install/elasticsearch/es_entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-	
 			
 init_datadir() {
 	echo "Initializing Elasticsearch data dir..."
@@ -17,6 +15,7 @@ init_datadir() {
 		else
 			echo "Existing environment, not copying anything to mounted volume..."
 		fi
+		
 	else
 		echo "!!! Data volume does not exist !!!"
 		exit 1
@@ -34,7 +33,41 @@ init_configdir() {
 	fi
 }
 
+configure_aws() {
+	# Get EC2 host's private ip, so that Elasticsearch nodes can find each other
+	AWS_PRIVATE_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+	if [[ ! -z $AWS_PRIVATE_IP ]]; then 
+		set -- "$@" --network.publish_host=$AWS_PRIVATE_IP
+	fi
+}
+
+set_permissions() {
+	# Change the ownership of ES data and home folders to elasticsearch
+	chown -R elasticsearch:elasticsearch ${DATA_VOLUME}
+	chown -R elasticsearch:elasticsearch ${CONFIG_VOLUME}
+	chown -R elasticsearch:elasticsearch /elasticsearch
+	chown -R elasticsearch:elasticsearch ${LOG_VOLUME}
+}
+
+
+
 echo "*** Initializing Elasticsearch ***"
- 
+
+# Add elasticsearch as command if needed (if first character provided by user is a '-', then only options are provided and not the 'elasticsearch' binary)
+if [ "${1:0:1}" = '-' ]; then
+        set -- /elasticsearch/bin/elasticsearch "$@"
+fi
+
 init_datadir
 init_configdir
+configure_aws
+set_permissions
+
+# Run elasticsearch bin as elasticsearch user
+if [ "$1" = '/elasticsearch/bin/elasticsearch' ]; then
+    exec sudo -u elasticsearch -- "$@"
+fi 
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
