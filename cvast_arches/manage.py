9c5a7cb06cc331464275d0a6dce56b739ebb7577
Vincent <meijer.vincent@gmail.com>
2016-08-02 13:28:52 -0400

    Refactoring: - Changed Django custom app/module name from 'arches_hip_cvast' to 'cvast_arches' - Changed root folder in Docker container to 'cvast_web'

--- cvast_arches/manage.py
+++ cvast_arches/manage.py
@@ -23,7 +23,7 @@ from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arches_hip_cvast.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cvast_arches.settings")
 
     execute_from_command_line(sys.argv)
 
--- install/obsolete/init_env.sh
+++ install/obsolete/init_env.sh
@@ -2,4 +2,4 @@
 su postgres --command "/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf" &
 /elasticsearch/bin/elasticsearch &
 sleep 10
-python /arches-cvast/arches_hip_cvast/manage.py runserver 0.0.0.0:8000
+python /cvast_web/cvast_arches/manage.py runserver 0.0.0.0:8000
