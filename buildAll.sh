11fb750b3cf5293fb83d9463eef55d4b88b90870
Vincent <meijer.vincent@gmail.com>
2016-08-02 13:20:21 -0400

    Refactoring: changed tags in docker images

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker build -f Dockerfile-arches-complete -t cvast/arches-complete ./  
-sudo docker build -f Dockerfile-web -t cvast/cvast-web ./
-sudo docker build -f Dockerfile-db -t cvast/cvast-db ./
-sudo docker build -f Dockerfile-elasticsearch -t cvast/cvast-elasticsearch ./
+sudo docker build -t cvast-build.eastus.cloudapp.azure.com:5000/arches-complete -f Dockerfile-arches-complete . 
+sudo docker build -f Dockerfile-web -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-web ./
+sudo docker build -f Dockerfile-db -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-db ./
+sudo docker build -f Dockerfile-elasticsearch -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-elasticsearch ./
