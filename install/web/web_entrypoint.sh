0b21cb936f2e86eddc00bc72d199faa36d18e76c
Vincent <meijer.vincent@gmail.com>
2016-08-29 15:35:32 -0400

    Ref #6 Added / to env variable path

--- install/web/web_entrypoint.sh
+++ install/web/web_entrypoint.sh
@@ -2,15 +2,15 @@
 set_password() {
 	if [[ ${IS_CLEAN_ENV} == True ]]; then
 		echo "Clean environment. Setting Django password..."
-		cd /${WEB_ROOT}/${WEB_APP_NAME}
-		python -u manage.py setPassword
+		cd ${WEB_ROOT}/${WEB_APP_NAME}
+		python manage.py setPassword
 	else
 		echo "Existing environment, not setting Django password..."
 	fi
 }
 
 run_django_server() {
-	exec python manage.py runserver 0.0.0.0:8000
+	exec python ${WEB_ROOT}/${WEB_APP_NAME}/manage.py runserver 0.0.0.0:8000
 }
 
 
