64f32293a3fdd0698645057e3d27fdd87a58a19d
Vincent <meijer.vincent@gmail.com>
2016-09-01 12:11:13 -0400

    Ref #6 Added script parameter to force deployment of specific apps. Added some local variables.

--- install/jenkinsBuild.sh
+++ install/jenkinsBuild.sh
@@ -6,9 +6,13 @@
 
 PREVIOUS_BUILD=`expr $BUILD_NUMBER - 1`
 OLD_IMAGE_BUILD=`expr $BUILD_NUMBER - 7` # For cleaning up old junk
-FORCE_DEPLOY=$1
 
-
+# Forced deployment possible through script parameters:
+while [[ $# -gt 0 ]]
+do
+	DEPLOY_THESE_APPS=("$DEPLOY_THESE_APPS" "$1")
+	shift
+done
 
 ####Functions####################################################################################################################
 
@@ -16,8 +20,9 @@ FORCE_DEPLOY=$1
 # Parameters: 
 # $1 = app (web, db, elasticsearch, nginx)
 build_image() {
-	echo "Building Docker image:  $1:$BUILD_NUMBER"
-	sudo docker build -f Dockerfile-$1 -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$BUILD_NUMBER .
+	local APP_NAME=$1
+	echo "Building Docker image:  $APP_NAME:$BUILD_NUMBER"
+	sudo docker build -f Dockerfile-$APP_NAME -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$BUILD_NUMBER .
 }
 
 
@@ -25,9 +30,10 @@ build_image() {
 # $1 = app (web, db, elasticsearch, nginx)
 # Cleanup old images, keep latest 7
 cleanup_old_image() {
-	if [[ $(sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$OLD_IMAGE_BUILD 2> /dev/null ) ]]; then
-		echo "Removing old image:  $1:$OLD_IMAGE_BUILD"
-		sudo docker rmi cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$OLD_IMAGE_BUILD
+	local APP_NAME=$1
+	if [[ $(sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$OLD_IMAGE_BUILD 2> /dev/null ) ]]; then
+		echo "Removing old image:  $APP_NAME:$OLD_IMAGE_BUILD"
+		sudo docker rmi cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$OLD_IMAGE_BUILD
 	fi
 }
 
@@ -35,33 +41,56 @@ cleanup_old_image() {
 # Parameters: 
 # $1 = app (web, db, elasticsearch, nginx)
 push_to_registry() {
-	echo "Pushing to private Docker registry:  $1:$BUILD_NUMBER "
-	sudo docker push cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$BUILD_NUMBER
+	local APP_NAME=$1
+	echo "Pushing to private Docker registry:  $APP_NAME:$BUILD_NUMBER "
+	sudo docker push cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$BUILD_NUMBER
+}
+
+
+# parameters: 
+# $1 = app (web, db, elasticsearch, nginx)
+prepare_deploy_image() {
+	local APP_NAME=$1
+	# See if old image exists.
+	if [[ $(sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$PREVIOUS_BUILD 2> /dev/null) ]]; then
+		local OLD_IMAGE=`sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$PREVIOUS_BUILD`
+	fi
+	
+	local NEW_IMAGE=`sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$APP_NAME:$BUILD_NUMBER`
+	
+	# Test if deployment is forced through script parameters
+	if [[ ! -z $DEPLOY_THESE_APPS ]]; then 
+		if [[ $(array_contains_element $APP_NAME $DEPLOY_THESE_APPS) ]]; then
+			deploy_image $APP_NAME
+		fi
+	# Else, deploy only if there are no older images or if older image is different. Efficiency!
+	elif [[ -z $OLD_IMAGE ]] || [[ "$OLD_IMAGE" != "$NEW_IMAGE" ]] || ; then
+		deploy_image $APP_NAME
+	fi
 }
 
 
 # parameters: 
 # $1 = app (web, db, elasticsearch, nginx)
 deploy_image() {
-	# See if old image exists.
-	if [[ $(sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$PREVIOUS_BUILD 2> /dev/null) ]]; then
-		local OLD_IMAGE=`sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$PREVIOUS_BUILD`
-	fi
-	
-	local NEW_IMAGE=`sudo docker images -q cvast-build.eastus.cloudapp.azure.com:5000/cvast-$1:$BUILD_NUMBER`
-	
-	# Deploy only if there are no older images or if older image is different. Efficiency!
-	if [[ -z $OLD_IMAGE ]] || [[ "$OLD_IMAGE" != "$NEW_IMAGE" ]] || [[ "$FORCE_DEPLOY" == True ]]; then
-		echo "Deploying to AWS:  $1:$BUILD_NUMBER"
-		sudo docker run cvast-build.eastus.cloudapp.azure.com:5000/cvast-arches-deploy -c $BUILD_NUMBER -e test -a $1
-	fi
+	echo "Deploying to AWS:  $APP_NAME:$BUILD_NUMBER"
+	sudo docker run cvast-build.eastus.cloudapp.azure.com:5000/cvast-arches-deploy -c $BUILD_NUMBER -e test -a $APP_NAME
 }
 
+
+# Check if value is in array
+array_contains_element() {
+	local e
+	for e in "${@:2}"; do 
+		[[ "$e" == "$1" ]] && return 0; 
+	done
+	return 1
+}
 
 #################################################################################################################################
 
 
-
+	
 ### Build all images
 sudo docker build -f Dockerfile-arches-complete -t cvast-build.eastus.cloudapp.azure.com:5000/arches-complete .
 build_image web
@@ -105,5 +134,5 @@ push_to_registry nginx
 sudo docker pull cvast-build.eastus.cloudapp.azure.com:5000/cvast-arches-deploy
 deploy_image web
 deploy_image nginx
-deploy_image db
-deploy_image elasticsearch
+# deploy_image db
+# deploy_image elasticsearch
