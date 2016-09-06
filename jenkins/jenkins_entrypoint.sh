020382925c7336c330b2803fb00b52c272a5ce55
Vincent <meijer.vincent@gmail.com>
2016-09-06 12:10:30 -0400

    Ref #6 Permission fixes in Jenkins Docker image

--- jenkins/jenkins_entrypoint.sh
+++ jenkins/jenkins_entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo chmod 755 /usr/bin/docker
+sudo chmod 755 /var/jenkins_home/.docker/config.json
+sudo chmod 777 /var/run/docker.sock
 
 exec /bin/tini -- /usr/local/bin/jenkins.sh
