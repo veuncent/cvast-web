99bc91189aaa142e13dec56eb74a47ba61a9bca2
Vincent <meijer.vincent@gmail.com>
2016-09-11 15:04:21 -0400

    Handy script to build all images. Removed sudo. Added nginx

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo docker build -t cvast-build.eastus.cloudapp.azure.com:5000/arches-complete -f Dockerfile-arches-complete . 
-sudo docker build -f Dockerfile-web -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-web ./
-sudo docker build -f Dockerfile-db -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-db ./
-sudo docker build -f Dockerfile-elasticsearch -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-elasticsearch ./
+docker build -t cvast-build.eastus.cloudapp.azure.com:5000/arches-complete -f Dockerfile-arches-complete . 
+docker build -f Dockerfile-web -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-web ./
+docker build -f Dockerfile-db -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-db ./
+docker build -f Dockerfile-elasticsearch -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-elasticsearch ./
+docker build -f Dockerfile-nginx -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-nginx ./
