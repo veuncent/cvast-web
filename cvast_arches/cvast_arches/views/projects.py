9d101a9d3563b6f22ff53a1d2bd6ad41a5625923
Vincent <meijer.vincent@gmail.com>
2016-09-17 11:12:31 -0400

    Ref #53 Added proper URL structure for Projects. Moved La Mancha and Paris into their own folder + url structure. Fixed links. Added Motilla and Nueva as subprojects of La Mancha. Included La Mancha footer and generic footer

--- cvast_arches/cvast_arches/views/projects.py
+++ cvast_arches/cvast_arches/views/projects.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-
+from django.template import TemplateDoesNotExist
+from django.http import Http404
 
 def index(request):
     return render_to_response('projects/index.htm', {
@@ -11,25 +12,25 @@ def index(request):
 
 
 def la_mancha(request):
-    return render_to_response('projects/la-mancha.htm', {
+    return render_to_response('projects/la-mancha/index.htm', {
         # 'main_script': 'index',
         'active_page': 'Projects',
     },
         context_instance=RequestContext(request))
 
-def la_motilla_del_azuer(request):
-    return render_to_response('projects/la-motilla-del-azuer.htm', {
-        # 'main_script': 'index',
-        'active_page': 'Projects',
-    },
-        context_instance=RequestContext(request))
+def la_mancha_resource(request, resource_name="calatrava-la-nueva"):
+    try:
+        return render_to_response('projects/la-mancha/%s.htm' % resource_name, {
+            # 'main_script': 'index',
+            'active_page': 'Projects',
+        },
+            context_instance=RequestContext(request))
+    except TemplateDoesNotExist:
+        raise Http404()
 
 def paris_nhm(request):
-    return render_to_response('projects/paris-nhm.htm', {
+    return render_to_response('projects/paris-nhm/index.htm', {
         # 'main_script': 'index',
         'active_page': 'Projects',
     },
         context_instance=RequestContext(request))
-
-
-
