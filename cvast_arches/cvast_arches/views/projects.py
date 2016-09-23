14105e485cc6d2cf69a94bff018b2b8fcad76444
Vincent <meijer.vincent@gmail.com>
2016-09-23 10:36:58 -0400

    Ref #54 Changed how javascript file is loaded (now in python view methods in the 'main_script' property)

--- cvast_arches/cvast_arches/views/projects.py
+++ cvast_arches/cvast_arches/views/projects.py
@@ -3,30 +3,33 @@ from django.shortcuts import render_to_response
 from django.template import TemplateDoesNotExist
 from django.http import Http404
 
+
 def index(request):
     return render_to_response('projects/index.htm', {
-        # 'main_script': 'index',
+        'main_script': 'cvast-video',
         'active_page': 'Projects',
     },
         context_instance=RequestContext(request))
 
+
 def project_index(request, project_name):
     try:
-        project_name = project_name.replace ("-", "_")
+        project_name = project_name.replace("-", "_")
         return render_to_response('projects/%s/index.htm' % project_name, {
-            # 'main_script': 'index',
+            'main_script': 'cvast-video',
             'active_page': 'Projects',
         },
             context_instance=RequestContext(request))
     except TemplateDoesNotExist:
         raise Http404()
 
+
 def subproject(request, project_name, resource_name):
     try:
-        project_name = project_name.replace ("-", "_")
-        filename = resource_name.replace ("-", "_")
+        project_name = project_name.replace("-", "_")
+        filename = resource_name.replace("-", "_")
         return render_to_response('projects/%s/%s.htm' % (project_name, filename), {
-            # 'main_script': 'index',
+            'main_script': 'cvast-video',
             'active_page': 'Projects',
         },
             context_instance=RequestContext(request))
