3fd48d2d2a3177ac7b0f18b601020b25f47ca59c
Vincent <meijer.vincent@gmail.com>
2016-10-12 11:18:23 -0400

    Removed obsolete scripts

--- buildAll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build -t cvast-build.eastus.cloudapp.azure.com:5000/arches-complete -f Dockerfile-arches-complete . 
-docker build -f Dockerfile-web -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-web ./
-docker build -f Dockerfile-db -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-db ./
-docker build -f Dockerfile-elasticsearch -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-elasticsearch ./
-docker build -f Dockerfile-nginx -t cvast-build.eastus.cloudapp.azure.com:5000/cvast-nginx ./
--- buildBaseImage.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t cvast-build.eastus.cloudapp.azure.com:5000/arches-complete -f Dockerfile-arches-complete .
