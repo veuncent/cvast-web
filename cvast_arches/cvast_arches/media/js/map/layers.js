08e7967bdb5623198c26c01009eae2c991f0c2ee
Vincent <meijer.vincent@gmail.com>
2016-11-01 16:03:49 -0400

    DEV-28 Changed http --> https. Also removed obsolete app_cvast.css

--- cvast_arches/cvast_arches/media/js/map/layers.js
+++ cvast_arches/cvast_arches/media/js/map/layers.js
@@ -12,7 +12,7 @@ define([
         icon: 'fa fa-bookmark-o',
         infoContent: layerI18n.exampleContent,
         layer: new ol.layer.Tile({
-            source: new ol.source.XYZ({url: 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'})
+            source: new ol.source.XYZ({url: 'https://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'})
         })
     }));
 
