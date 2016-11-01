12c990ba6e1048f9ff8e47db1cff861d9495f70a
Vincent <meijer.vincent@gmail.com>
2016-11-01 10:54:36 -0400

    DEV-28 Added Certbot to NginX container. Entrypoint downloads certificate for specified domain. Prepared docker-compose.yml for Jenkins build server domain name.

--- install/nginx/nginx_entrypoint.sh
+++ install/nginx/nginx_entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+DOMAIN_NAME=${DOMAIN_NAME}
+LETSENCRYPT_EMAIL=${LETSENCRYPT_EMAIL}
+
 sed -i "s/<django_host>/${DJANGO_HOST}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/<domain_name>/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
+
+mkdir -p /var/www/${DOMAIN_NAME}
+
+if [[ ${GET_NEW_CERTIFICATE} == True ]]; then
+	certbot certonly \
+		--agree-tos \
+		--email ${LETSENCRYPT_EMAIL} \
+		--webroot \
+		-w /var/www/${DOMAIN_NAME} \
+		-d ${DOMAIN_NAME} \
+		${ADDITIONAL_CERTBOT_PARAMS}
+fi
+
 exec nginx -g 'daemon off;'
