d0a8c43b11892704de859ce59d5493f9b586b11d
Vincent <meijer.vincent@gmail.com>
2016-11-02 10:43:50 -0400

    DEV-28 Remodeled how Nginx is started before Certbot is started (with dummy certificates). Also removed localhost from nginx conf server_name

--- install/nginx/nginx_entrypoint.sh
+++ install/nginx/nginx_entrypoint.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-DOMAIN_NAME=${DOMAIN_NAME}
+# For Letsencrypt / Certbot verification
 LETSENCRYPT_EMAIL=${LETSENCRYPT_EMAIL}
 
+# Set name of Django container
+echo "Initializing NginX to run on ${DOMAIN_NAME} and serve as reverse proxy for ${DJANGO_HOST}..."
 sed -i "s/<django_host>/${DJANGO_HOST}/g" /etc/nginx/conf.d/default.conf
 sed -i "s/<domain_name>/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
 
-mkdir -p /var/www/${DOMAIN_NAME}
 
 if [[ ${GET_NEW_CERTIFICATE} == True ]]; then
+	echo "GET_NEW_CERTIFICATE = True, so downloading new certificate from LetsEncrypt"
+		
+	# Temporary dummy certs so Nginx will startup without complaining
+	mkdir -p /etc/letsencrypt/live/${DOMAIN_NAME}
+	cp ${INSTALL_DIR}/fullchain.pem /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem;
+	cp ${INSTALL_DIR}/privkey.pem /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem;
+	
+	mkdir -p /var/www/${DOMAIN_NAME}
+	
+	echo "Temporarilly starting NginX in order to let Certbot verify something is running on port 80..."
+	service nginx start
+	
+	echo "Starting Certbot to download certificate"
 	certbot certonly \
 		--agree-tos \
 		--email ${LETSENCRYPT_EMAIL} \
@@ -16,6 +30,15 @@ if [[ ${GET_NEW_CERTIFICATE} == True ]]; then
 		-w /var/www/${DOMAIN_NAME} \
 		-d ${DOMAIN_NAME} \
 		${ADDITIONAL_CERTBOT_PARAMS}
+	
+	echo "Stopping Nginx in order to reload config and run it in the foreground..."
+	service nginx stop
+	
+	echo "Running Nginx on ${DOMAIN_NAME} in the foreground"
+	exec nginx -g 'daemon off;'
+	
+else
+	echo "Running Nginx on ${DOMAIN_NAME} without downloading new certificate"
+	exec nginx -g 'daemon off;' 
 fi
 
-exec nginx -g 'daemon off;'
