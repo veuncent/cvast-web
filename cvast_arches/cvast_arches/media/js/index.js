c176bccd47034e8276e1b0180bdf56624add1bc7
Vincent <meijer.vincent@gmail.com>
2017-01-23 17:38:47 -0500

    DEV-154 Replaced Layerslider with Flexslider for homepage images.

--- cvast_arches/cvast_arches/media/js/index.js
+++ cvast_arches/cvast_arches/media/js/index.js
@@ -2,28 +2,12 @@ require([
     'jquery',
     'arches',
     'easing',
-    'plugins/layer_slider/jQuery/jquery-transit-modified',
-    'plugins/layer_slider/js/layerslider.transitions',
-    'plugins/layer_slider/js/layerslider.kreaturamedia.jquery',
     'flexslider'
 ], function($, arches) {
     $(document).ready(function() {
-        $('#photo-flexslider').flexslider({
+        $('.flexslider').flexslider({
             animation: "slide",
-            controlNav: false,
-            animationLoop: false,
-            itemWidth: 80,
-            itemMargin: 0
-        });
-
-        $('#layerslider').layerSlider({
-            skinsPath: arches.urls.media + 'plugins/layer_slider/skins/',
-            skin: 'fullwidth',
-            thumbnailNavigation: false,
-            hoverPrevNext: true,
-            responsive: true,
-            responsiveUnder: 960,
-            sublayerContainer: 960
+            controlsContainer: '.flexslider'
         });
     });
 });
