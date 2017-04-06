9933f09387aa269062bf5c003840beec8b1e476a
Vincent <meijer.vincent@gmail.com>
2017-04-06 15:15:27 -0400

    DEV-264 Refactor About Us urls

--- cvast_arches/cvast_arches/views/about_us.py
+++ cvast_arches/cvast_arches/views/about_us.py
@@ -1,32 +1,14 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.template import TemplateDoesNotExist
+from django.http import Http404
 
-def mission(request):
-    return render_to_response('about_us/mission.htm', {
-        # 'main_script': 'index',
-        'active_page': 'About_us',
-    },
-        context_instance=RequestContext(request))
-
-def people(request):
-    return render_to_response('about_us/people.htm', {
-        # 'main_script': 'index',
-        'active_page': 'About_us',
-    },
-        context_instance=RequestContext(request))
-
-
-def partners(request):
-    return render_to_response('about_us/partners.htm', {
-        # 'main_script': 'index',
-        'active_page': 'About_us',
-    },
-        context_instance=RequestContext(request))
-
-
-def technology(request):
-    return render_to_response('about_us/technology.htm', {
-        # 'main_script': 'index',
-        'active_page': 'About_us',
-    },
-        context_instance=RequestContext(request))
+def about_us_subpage(request, about_us_name):
+    try:
+        about_us_name = about_us_name.replace("-", "_")
+        return render_to_response('about_us/%s.htm' % about_us_name, {
+            'active_page': 'About_us',
+        },
+            context_instance=RequestContext(request))
+    except TemplateDoesNotExist:
+        raise Http404()
