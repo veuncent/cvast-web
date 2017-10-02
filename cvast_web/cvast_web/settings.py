ce404a7d189c6ee57ad1b86280ece78fac30bff9
Vincent <meijer.vincent@gmail.com>
2017-10-02 12:05:29 -0400

    DEV-215 Add main_website app to INSTALLED_APPS

--- cvast_web/cvast_web/settings.py
+++ cvast_web/cvast_web/settings.py
@@ -83,7 +83,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_website.apps.MainWebsiteConfig'
+    'main_website'
 ]
 
 MIDDLEWARE = [
