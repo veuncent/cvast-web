a5548351760efbb3b6e902b83771eb37be3bb9e6
Vincent <meijer.vincent@gmail.com>
2017-10-07 19:22:40 -0400

    DEV-397 Initial model for Home. Initial template for Home. Move base templates to website project. Add wagtailcore_tags to included templates. Set urls to wagtail format. Add base template and error templates to project.

--- cvast_web/website/models.py
+++ cvast_web/website/models.py
@@ -3,4 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+
+
+class HomePage(Page):
+    template = 'home/home_index.htm'
+
+    intro_title = models.CharField(max_length=100, default="Center for Virtualization and Applied Spatial Technologies")
+    intro_text = RichTextField(blank=True)
+    intro_button = models.CharField(max_length=40, default="Learn More")
+
+    news_summary_title = models.CharField(max_length=120, default="News")
+    news_summary_link = models.CharField(max_length=50, default="See all News items")
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro_title', classname="full"),
+        FieldPanel('intro_text', classname="full"),
+        FieldPanel('intro_button', classname="full"),
+        FieldPanel('news_summary_title', classname="full"),
+        FieldPanel('news_summary_link', classname="full"),
+    ]
+
