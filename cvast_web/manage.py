9ab12a5e891114089a8e18a85bd00711ffe75bdd
Vincent <meijer.vincent@gmail.com>
2017-10-10 10:43:34 -0400

    Only attach debugger if not already attached

--- cvast_web/manage.py
+++ cvast_web/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import ptvsd
 from cvast_web import settings
 
 if __name__ == "__main__":
@@ -22,13 +23,13 @@ if __name__ == "__main__":
         raise
 
     if settings.REMOTE_DEBUG:
-        import ptvsd
-        debug_secret = settings.get_optional_env_variable("DEBUG_SECRET")
-        ptvsd.enable_attach(debug_secret, address =('0.0.0.0', 3000))
-        if ptvsd.is_attached:
-            print "Attached debugger"
-        else:
-            print "Not attached"
+        if not ptvsd.is_attached:
+            debug_secret = settings.get_optional_env_variable("DEBUG_SECRET")
+            ptvsd.enable_attach(debug_secret, address =('0.0.0.0', 3000))
+            if ptvsd.is_attached:
+                print "Attached debugger"
+            else:
+                print "Not attached"
 
 
     execute_from_command_line(sys.argv)
