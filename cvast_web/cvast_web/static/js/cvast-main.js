4ca85e2ecde65ea35558e8ce7f925a8286d1cbe4
Vincent <meijer.vincent@gmail.com>
2017-10-12 09:55:06 -0400

    DEV_397 Make external links open in new browser window

--- cvast_web/cvast_web/static/js/cvast-main.js
+++ cvast_web/cvast_web/static/js/cvast-main.js
@@ -5,6 +5,10 @@ require([
 
     $(document).ready(function() {
         cvast_video.loadVideoplayer();
+
+        // Make all external links open in new browser window
+        $('a[href^="http://"]').attr('target', '_blank');
+        $('a[href^="https://"]').attr('target', '_blank');
     });
 
 });
