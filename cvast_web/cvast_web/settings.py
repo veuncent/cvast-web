8413bcc80dc1988c7e16286a8ed6cb6959bb8509
Vincent <meijer.vincent@gmail.com>
2017-10-19 12:21:05 -0400

    DEV-397 Make CONSOLE_LOGGING env vairalbe optional

--- cvast_web/cvast_web/settings.py
+++ cvast_web/cvast_web/settings.py
@@ -240,7 +240,7 @@ LOGGING = {
     }
 }
 
-if get_env_variable('CONSOLE_LOGGING'):
+if get_optional_env_variable('CONSOLE_LOGGING'):
     for logger in LOGGING['loggers']:
         LOGGING['loggers'][logger]['handlers'] += ['console']
 
